748691072b02a0c963f9b3f11132d9236c88b338
almogaya1315 <liormatsliah1985@gmail.com>
2017-11-06 23:30:23 +0200

    flow

--- SquadManager/SquadManager.UI/sql/SQLQuery.sql
+++ SquadManager/SquadManager.UI/sql/SQLQuery.sql
@@ -4,4 +4,18 @@ FROM			TblManagers managers
 JOIN			TblNations nations
 ON				managers.Manager_Nationality = nations.Nation_Id
 
+SELECT *
+FROM TblTeams
+
+SELECT *
+FROM TblNations
+
+SELECT *
+FROM TblCities
+
+SELECT *
+FROM TblSports
+
+SELECT *
+FROM TblTeams
 
