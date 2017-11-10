3e8229f2e8e6ea8b80afd108d98bf539c5f163f5
almogaya1315 <liormatsliah1985@gmail.com>
2017-11-10 15:52:44 +0200

    flow

---
+++ SquadManager/SquadManager.UI/Controls/RadioButton/TeamRadioButton.cs
@@ -0,0 +1,26 @@
+﻿using SquadManager.UI.TeamDetails.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SquadManager.UI.Controls.RadioButton
+{
+    public class TeamRadioButton : System.Windows.Controls.RadioButton
+    {
+        public TeamViewModel SelectedTeam
+        {
+            get { return (TeamViewModel)GetValue(SelectedTeamProperty); }
+            set { SetValue(SelectedTeamProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedTeamProperty =
+            DependencyProperty.Register("SelectedTeam", 
+                typeof(TeamViewModel), typeof(TeamRadioButton), 
+                new PropertyMetadata(null));
+
+
+    }
+}
