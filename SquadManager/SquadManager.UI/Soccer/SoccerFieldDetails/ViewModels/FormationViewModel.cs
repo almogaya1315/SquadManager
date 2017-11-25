bc28e79a215766af2d0b3f208bec501ba9a00d4a
almogaya1315 <liormatsliah1985@gmail.com>
2017-11-25 17:15:52 +0200

    flow

--- SquadManager/SquadManager.UI/Soccer/SoccerFieldDetails/ViewModels/FormationViewModel.cs
+++ SquadManager/SquadManager.UI/Soccer/SoccerFieldDetails/ViewModels/FormationViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using SquadManager.UI.Base;
+using SquadManager.UI.Soccer.SoccerPlayerDetails.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,17 @@ namespace SquadManager.UI.Soccer.SoccerFieldDetails.ViewModels
     public class FormationViewModel : ViewModel
     {
         public string Name { get; set; }
+
+        public SoccerPlayerViewModel Player_1 { get; set; }
+        public SoccerPlayerViewModel Player_2 { get; set; }
+        public SoccerPlayerViewModel Player_3 { get; set; }
+        public SoccerPlayerViewModel Player_4 { get; set; }
+        public SoccerPlayerViewModel Player_5 { get; set; }
+        public SoccerPlayerViewModel Player_6 { get; set; }
+        public SoccerPlayerViewModel Player_7 { get; set; }
+        public SoccerPlayerViewModel Player_8 { get; set; }
+        public SoccerPlayerViewModel Player_9 { get; set; }
+        public SoccerPlayerViewModel Player_10 { get; set; }
+        public SoccerPlayerViewModel Player_11 { get; set; }
     }
 }
--- SquadManager/SquadManager.UI/Soccer/SoccerFieldDetails/ViewModels/SoccerFieldDetailsViewModel.cs
+++ SquadManager/SquadManager.UI/Soccer/SoccerFieldDetails/ViewModels/SoccerFieldDetailsViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using SquadManager.UI.Base;
+using SquadManager.UI.Models;
+using SquadManager.UI.SharedViewModels;
+using SquadManager.UI.Soccer.SoccerPlayerDetails.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +12,39 @@ namespace SquadManager.UI.Soccer.SoccerFieldDetails.ViewModels
 {
     public class SoccerFieldDetailsViewModel : ViewModel, IChangeable
     {
-        public List<FormationViewModel> Formations { get; set; }
+        private readonly IChangeManager _changeManager;
+        private Team _teamModel;
 
-        public SoccerFieldDetailsViewModel()
+        public List<FormationViewModel> Formations { get; set; }
+        public FormationViewModel SelectedFormation { get; set; }
+
+        public SoccerFieldDetailsViewModel(Team team, IChangeManager changeManager, CollectionFactory collections)
         {
+            _changeManager = changeManager;
+            _teamModel = team;
+
+            Collections = collections;
+
             Formations = new List<FormationViewModel>()
             {
-                new FormationViewModel() { Name = "4-4-2" },
+                new FormationViewModel() { Name = "4-4-2", Player_1 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.GK), Collections, _changeManager),
+                                                           Player_2 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.LB), Collections, _changeManager),
+                                                           Player_3 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.CB), Collections, _changeManager),
+                                                           Player_4 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.CB), Collections, _changeManager),
+                                                           Player_5 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.RB), Collections, _changeManager),
+                                                           Player_6 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.LW), Collections, _changeManager),
+                                                           Player_7 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.CM), Collections, _changeManager),
+                                                           Player_8 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.CM), Collections, _changeManager),
+                                                           Player_9 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.RW), Collections, _changeManager),
+                                                           Player_10 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.CAM), Collections, _changeManager),
+                                                           Player_11 = new SoccerPlayerViewModel(_teamModel.Squad.FirstOrDefault(p => p.Position.Role == PositionRole.CF), Collections, _changeManager)},
                 new FormationViewModel() { Name = "4-4-2" },
                 new FormationViewModel() { Name = "4-4-2" },
                 new FormationViewModel() { Name = "4-4-2" },
                 new FormationViewModel() { Name = "4-4-2" },
             };
+
+            SelectedFormation = Formations.First();
         }
 
         public void Changed(ChangeArgs args)
