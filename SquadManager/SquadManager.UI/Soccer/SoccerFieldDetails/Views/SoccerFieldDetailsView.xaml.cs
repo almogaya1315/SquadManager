1401709c66fdf7579ada89e2674fc9946d38c36a
almogaya1315 <liormatsliah1985@gmail.com>
2017-12-08 16:27:15 +0200

    flow

--- SquadManager/SquadManager.UI/Soccer/SoccerFieldDetails/Views/SoccerFieldDetailsView.xaml.cs
+++ SquadManager/SquadManager.UI/Soccer/SoccerFieldDetails/Views/SoccerFieldDetailsView.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -17,9 +18,26 @@ namespace SquadManager.UI.Soccer.SoccerFieldDetails.Views
 {
     public partial class SoccerFieldDetailsView : UserControl
     {
+        private Canvas _formationCanvas;
+        private ToggleButton _player;
+
         public SoccerFieldDetailsView()
         {
             InitializeComponent();
+
+            var editFormationControlTemplate = (ControlTemplate)Resources["EditFormationTemplate"];
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            _player = (ToggleButton)sender;
+            if (_player == null) return;
+            AddHandler(MouseMoveEvent, new MouseEventHandler(OnMouseMoveChanged));
+        }
+
+        private void OnMouseMoveChanged(object sender, MouseEventArgs args)
+        {
+            Canvas.SetLeft(_player, args.GetPosition(_formationCanvas).X - _player.ActualWidth / 2);
         }
     }
 }
