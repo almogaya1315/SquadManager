8673cf1838d917f381e1b7d4ee5f8f5efe322783
almogaya1315 <liormatsliah1985@gmail.com>
2017-12-08 20:46:14 +0200

    flow

--- SquadManager/SquadManager.UI/Soccer/SoccerFieldDetails/Views/SoccerFieldDetailsView.xaml.cs
+++ SquadManager/SquadManager.UI/Soccer/SoccerFieldDetails/Views/SoccerFieldDetailsView.xaml.cs
@@ -22,31 +22,60 @@ namespace SquadManager.UI.Soccer.SoccerFieldDetails.Views
     {
         private Canvas _formationCanvas;
         private ToggleButton _player;
+        private bool _playerDragged;
+        private MouseEventHandler _onMouseMoveChanged;
 
         public SoccerFieldDetailsView()
         {
             InitializeComponent();
+
+            
+            _onMouseMoveChanged = new MouseEventHandler(OnMouseMoveChanged);
         }
 
         private void ToggleButton_Click(object sender, RoutedEventArgs e)
         {
-            var editFormationControlTemplate = (ControlTemplate)Resources["EditFormationTemplate"];
-            var formationItemsControl = (ItemsControl)editFormationControlTemplate.FindName("FormationItemsControl", FormationContentControl);
-            _formationCanvas = XamlHelper.FindChild<Canvas>(this, "FormationCanvas"); 
-
             _player = (ToggleButton)sender;
             if (_player == null) return;
-            AddHandler(MouseMoveEvent, new MouseEventHandler(OnMouseMoveChanged));
+
+            if (!_playerDragged)
+            {
+                AddHandler(MouseMoveEvent, _onMouseMoveChanged);
+                _playerDragged = true;
+
+                // TODO: 
+                // raise change for start of edit formation mode
+            }
+            else
+            {
+                RemoveHandler(MouseMoveEvent, _onMouseMoveChanged);
+                _playerDragged = false;
+
+                // TODO:
+                // update DB
+                // raise change for end of edit formation mode
+            }
         }
 
         private void OnMouseMoveChanged(object sender, MouseEventArgs args)
         {
-            var mouseX = args.GetPosition(_formationCanvas).X;
-            var mouseY = args.GetPosition(_formationCanvas).Y;
+            var mouseX = args.GetPosition(FormationCanvas()).X;
+            var mouseY = args.GetPosition(FormationCanvas()).Y;
 
             var playerViewModel = (SoccerPlayerViewModel)_player.DataContext;
             playerViewModel.X = (int)(mouseX - _player.ActualWidth / 2);
             playerViewModel.Y = (int)(mouseY - _player.ActualHeight / 2);
+        }
+
+        private Canvas FormationCanvas()
+        {
+            if (_formationCanvas == null)
+            {
+                var editFormationControlTemplate = (ControlTemplate)Resources["EditFormationTemplate"];
+                var formationItemsControl = (ItemsControl)editFormationControlTemplate.FindName("FormationItemsControl", FormationContentControl);
+                _formationCanvas = XamlHelper.FindChild<Canvas>(this, "FormationCanvas");
+            }
+            return _formationCanvas;
         }
     }
 }
